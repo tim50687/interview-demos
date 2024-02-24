efd20462c97e0c9e1eb3e6cccd10b9e346d1c993
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2024-02-24 15:38:55 -0500

    test

---
+++ ui-pipelines/js/MockService.js
@@ -0,0 +1,13 @@
+var PipelineService =  {
+	getPipeline: function(id) {
+		return {
+		  "label": "My First Pipeline",
+		  "tags": ["Tag1", "Tag2"],
+		  "channel": "Quick Base"
+		}
+	},
+	savePipeline: function (pipelineJson) {
+		// Add the code here to call the API (or temporarily, just log pipelineJson to the console)
+		
+	}
+}
