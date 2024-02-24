0f2e0bb776e4382012130fc3b960ce028d321f4c
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2024-02-24 12:43:30 -0500

    set up adding country logic

--- ui/jen-ting-huang-interview-project/src/components/Field/components/TextArea.js
+++ ui/jen-ting-huang-interview-project/src/components/Field/components/TextArea.js
@@ -1,7 +1,54 @@
+import { useState } from "react";
+
 const TextArea = () => {
+  // State for the country list
+  const [country, setCountry] = useState([]);
+  // State for the country input
+  const [currentInput, setCurrentInput] = useState("");
+
+  // State for duplicate country warning
+  const [showDuplicateWarning, setShowDuplicateWarning] = useState(false);
+  // State for empty country warning
+  const [showEmptyWarning, setShowEmptyWarning] = useState(false);
+  console.log(currentInput);
+  console.log(country);
+
+  const handleKeyDown = (e) => {
+    // If the user presses the Enter key
+    if (e.key === "Enter") {
+      const entries = currentInput.split("\n");
+      // Add the current input to the country list
+      const newCountry = entries[entries.length - 1];
+
+      // If user didn't type anything, or include duplicate country
+      if (newCountry === "") {
+        setShowEmptyWarning(true);
+        setShowDuplicateWarning(false);
+        e.preventDefault();
+      } else if (country.includes(newCountry)) {
+        setShowDuplicateWarning(true);
+        setShowEmptyWarning(false);
+        e.preventDefault();
+      } else {
+        setCountry((prev) => [...prev, newCountry]);
+        // Hide warnings if successfully added
+        setShowDuplicateWarning(false);
+        setShowEmptyWarning(false);
+      }
+    }
+  };
+
   return (
     <div>
-      <textarea></textarea>
+      <textarea
+        value={currentInput} // force the value to be the currentInput
+        onChange={(e) => setCurrentInput(e.target.value)}
+        onKeyDown={handleKeyDown}
+      ></textarea>
+      {showDuplicateWarning && (
+        <p className="warning">You have already added this country</p>
+      )}
+      {showEmptyWarning && <p className="warning">Please enter a country</p>}
     </div>
   );
 };
