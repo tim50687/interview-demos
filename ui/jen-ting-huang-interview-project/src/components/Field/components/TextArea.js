935889515096d7de5e2516f5609f107a9d185d16
Jen Ting Huang <jentinghuang@Jens-MacBook-Pro.local>
2024-02-24 13:35:43 -0500

    added remove country logic

--- ui/jen-ting-huang-interview-project/src/components/Field/components/TextArea.js
+++ ui/jen-ting-huang-interview-project/src/components/Field/components/TextArea.js
@@ -10,8 +10,8 @@ const TextArea = () => {
   const [showDuplicateWarning, setShowDuplicateWarning] = useState(false);
   // State for empty country warning
   const [showEmptyWarning, setShowEmptyWarning] = useState(false);
-  console.log(currentInput);
-  console.log(country);
+  // State for adding and removing country
+  const [notification, setNotification] = useState("");
 
   const handleKeyDown = (e) => {
     // If the user presses the Enter key
@@ -34,7 +34,24 @@ const TextArea = () => {
         // Hide warnings if successfully added
         setShowDuplicateWarning(false);
         setShowEmptyWarning(false);
+        // Notify user that the country has been added
+        setNotification(`Added ${newCountry} to the list`);
       }
+    }
+  };
+
+  const handleChange = (e) => {
+    // Normal case, if user types something
+    setCurrentInput(e.target.value);
+    setShowDuplicateWarning(false);
+    setShowEmptyWarning(false);
+
+    // After the change rerender, if the country is removed, remove it from the list
+    const entries = e.target.value.split("\n");
+
+    if (entries.length - 1 < country.length) {
+      setNotification(`Successfully removed country from the list`);
+      setCountry(entries.slice(0, entries.length - 1));
     }
   };
 
@@ -42,13 +59,15 @@ const TextArea = () => {
     <div>
       <textarea
         value={currentInput} // force the value to be the currentInput
-        onChange={(e) => setCurrentInput(e.target.value)}
+        onChange={handleChange}
         onKeyDown={handleKeyDown}
+        rows={10}
       ></textarea>
       {showDuplicateWarning && (
         <p className="warning">You have already added this country</p>
       )}
       {showEmptyWarning && <p className="warning">Please enter a country</p>}
+      {notification && <p className="notification">{notification}</p>}
     </div>
   );
 };
